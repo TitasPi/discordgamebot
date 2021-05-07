0a9ea072b9a3953026d0b972831e94cd22bbfd60
TitasPi <titasp@pixylt.com>
2021-05-07 11:18:11 +0300

    updated craft command

--- commands/craft.js
+++ commands/craft.js
@@ -17,71 +17,60 @@ exports.execute = async function(message, commandArgs, Users, Enemies, UserItems
 
     const user = await Users.findOne({ where: { user_id: message.author.id } });
 
-    if(item.name === 'Iron Axe' || item.name === 'Iron Pickaxe' || item.name === 'Copper Axe' || item.name === 'Copper Pickaxe') {
-        const metalItem = await getMetalItem(CurrencyShop, item.name);
-        const userMetalItem = await user.hasItem(metalItem, 2);
+    if(item.name === 'Iron Axe' || item.name === 'Iron Pickaxe' || item.name === 'Copper Axe' || item.name === 'Copper Pickaxe' || item.name === 'Fishing Rod') {
+        // First crafting component
         const logItem = await user.getUserLogs();
         const woodItem = await CurrencyShop.findOne({ where: { name: { [Op.like]: logItem } } });
         const userWoodItem = await user.hasItem(woodItem);
+
+        // Second crafting component
+        const secondComponent = await getSecondComponent(CurrencyShop, item.name);
+        const secondComponentAmount = await getSecondComponentAmount(item.name);
+        const userSecondComponent = await user.hasItem(secondComponent, secondComponentAmount);
+
         let missing = '';
-        if(userMetalItem && !userWoodItem) {
+        if(userSecondComponent && !userWoodItem) {
             missing = '1x Log';
         }
-        else if(!userMetalItem && !userWoodItem) {
-            missing = `1x Log, 2x ${metalItem.name}`;
+        else if(!userSecondComponent && !userWoodItem) {
+            missing = `1x Log, ${secondComponentAmount}x ${secondComponent.name}`;
         }
-        else if(!userMetalItem && userWoodItem) {
-            missing = `2x ${metalItem.name}`;
+        else if(!userSecondComponent && userWoodItem) {
+            missing = `${secondComponentAmount}x ${secondComponent.name}`;
         }
         if(missing !== '') {
             return message.channel.send(new Discord.MessageEmbed().setTitle('Crafting').setDescription(`You need to have \`${missing}\` to craft \`${getItemName(item)}\``));
         }
 
         await user.removeItem(woodItem);
-        await user.removeItem(metalItem, 2);
+        await user.removeItem(secondComponent, secondComponentAmount);
         await user.addItem(item);
         const xp = random(1, 5);
         user.crafting_skill += xp;
         user.save();
-        return message.channel.send(new Discord.MessageEmbed().setTitle('Crafting').setDescription(`You crafted 1x ${getItemName(item)} (used 1x ${getItemName(woodItem)}) and 2x ${getItemName(metalItem)}. Gained ${xp}XP`));
-    }
-    else if(item.name === 'Fishing Rod') {
-        const stringItem = await CurrencyShop.findOne({ where: { name: { [Op.like]: ':thread: String' } } });
-        const userItem1 = await user.hasItem(stringItem);
-        const logItem = await user.getUserLogs();
-        const woodItem = await CurrencyShop.findOne({ where: { name: { [Op.like]: logItem } } });
-        const userItem2 = await user.hasItem(woodItem);
-        let missing = '';
-        if(userItem1 && !userItem2) {
-            missing += '1x Log';
-        }
-        else if(!userItem1 && !userItem2) {
-            missing += `1x Log, 1x ${stringItem.name}`;
-        }
-        else if(!userItem1) {
-            missing += `1x ${stringItem.name}`;
-        }
-        if(missing !== '') {
-            return message.channel.send(new Discord.MessageEmbed().setTitle('Crafting').setDescription(`You need to have **${missing}** to craft **${getItemName(item)}**`));
-        }
-        await user.removeItem(woodItem);
-        await user.removeItem(stringItem);
-        await user.addItem(item);
-        const xp = random(1, 5);
-        user.crafting_skill += xp;
-        user.save();
-        return message.channel.send(new Discord.MessageEmbed().setTitle('Crafting').setDescription(`You crafted 1x **${getItemName(item)}** (used 1x **${getItemName(woodItem)}** and 2x **${getItemName(stringItem)}**). Gained ${xp}XP`));
+        return message.channel.send(new Discord.MessageEmbed().setTitle('Crafting').setDescription(`You crafted 1x **${getItemName(item)}** (used 1x **${getItemName(woodItem)}** and ${secondComponentAmount}x **${getItemName(secondComponent)}**). Gained ${xp}XP`));
     }
     else {
         return message.channel.send(new Discord.MessageEmbed().setTitle('Crafting').setDescription('I can\'t craft that'));
     }
 };
 
-async function getMetalItem(CurrencyShop, itemName) {
+async function getSecondComponent(CurrencyShop, itemName) {
     if(itemName === 'Iron Axe' || itemName === 'Iron Pickaxe') {
         return await CurrencyShop.findOne({ where: { name: { [Op.like]: 'Iron Ingot' } } });
     }
     if(itemName === 'Copper Axe' || itemName === 'Copper Pickaxe') {
         return await CurrencyShop.findOne({ where: { name: { [Op.like]: 'Copper Ingot' } } });
+    }
+    if(itemName === 'Fishing Rod') {
+        return await CurrencyShop.findOne({ where: { name: { [Op.like]: ':thread: String' } } });
+    }
+}
+async function getSecondComponentAmount(itemName) {
+    if(itemName === 'Iron Axe' || itemName === 'Iron Pickaxe' || itemName === 'Copper Axe' || itemName === 'Copper Pickaxe') {
+        return 2;
+    }
+    if(itemName === 'Fishing Rod') {
+        return 1;
     }
 }
